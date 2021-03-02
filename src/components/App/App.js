292af951e67e7cbbaff8fb19f9fb70ec5c468f17
Tony CABAYE <tony.cabaye@smile.fr>
2021-03-02 15:18:21 +0100

    lift the Article selected state up to the App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,11 @@
+import { useState } from 'react';
+
 import Article from "../Article/Article";
 import Header from "../Header/Header";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [selectedArticles, setSelectedArticles] = useState([]);
   const articles = [
     {
       title: "Article 1",
@@ -23,14 +27,41 @@ function App() {
     }
   ];
 
-  const list = articles.map(article =>
-    <Article article={article} key={article.id} />
+  function increment() {
+    setCounter(prevState => prevState + 1);
+  }
+
+  function toggleArticle(index) {
+    // Avec map
+    // setSelectedArticles(prevState => {
+    //   return prevState.map((state, i) => {
+    //     return index === i ? !state : state;
+    //   });
+    // });
+
+    // Avec clone
+    setSelectedArticles(prevState => {
+      const clone = [...prevState];
+      clone[index] = !clone[index];
+      return clone;
+    });
+  }
+
+  const list = articles.map((article, i) =>
+    <Article
+      article={article}
+      index={i}
+      key={article.id}
+      selected={selectedArticles[i]}
+      toggleArticle={toggleArticle}
+    />
   );
 
   return (
     <div className="App">
       <Header/>
       {list}
+      <button onClick={increment}>{counter}</button>
     </div>
   );
 }
--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -1,11 +1,19 @@
+
 import "./Article.css";
 
 function Article(props) {
-  const { article } = props;
+  const { article, index, selected, toggleArticle } = props;
   const { title, category, published } = article;
 
+  function handleClick() {
+    toggleArticle(index);
+  }
+
   return (
-    <div className="Article">
+    <div
+      className={'Article ' + (selected ? 'Article--selected' : '')}
+      onClick={handleClick}
+    >
       <div className="Article__title">{title}</div>
       <div>{category}</div>
       <div>{published ? 'Published' : 'Draft'}</div>
